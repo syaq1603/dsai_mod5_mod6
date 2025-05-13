65f97fbb454db3c0fb507c50adf78ede9e21c276
Rubiyah Biamin <syaq1603@gmail.com>
2025-05-13 12:53:01 +0000

    test

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET"]
+@app.route("/",methods=["GET","POST"])
+def index():
+
+return(render_template("index.html"))
+if __name__=="__main__"
+    app_run()
