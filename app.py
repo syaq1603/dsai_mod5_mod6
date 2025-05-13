a9461992ba5e5fa1d9d016e2ffd7408e62aaa026
Rubiyah Biamin <syaq1603@gmail.com>
2025-05-13 13:00:47 +0000

    1

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
 
-return(render_template("index.html"))
-if __name__=="__main__"
-    app_run()
+    return(render_template("index.html"))
+    if __name__=="__main__":
+        app_run()
