3ba6cf82bfdc675cb03119c7cc91d84bf1665512
Rubiyah Biamin <syaq1603@gmail.com>
2025-05-16 04:51:10 +0000

    4th commit

---
+++ README.md
@@ -0,0 +1,6 @@
+###Deploy GenAI application with Flask to render.com
+
+- There are 2 chatbots (Gemini 2.5 Flash and OpenAI GTP-4o) deployed to this Web Appication
+- Populate API keys securely to Github Codespace and Render.com using Environment Secrets
+- Response from Gemini page is presented in raw text format
+- Response from OpenAI page is presented in markdown/html format
--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 #gemini
+
+#gemini
+
 from flask import Flask,request,render_template
 import google.generativeai as genai
 import os
+
 gemini_api_key = os.environment["gemini_api_key"]
+
 genai.configure(api_key=gemini_api_key)
 model = genai.GenerativeModel("gemini-2.0-flash")
+
 app = Flask(__name__)
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
 @app.route("/gemini",methods=["GET","POST"])
 def gemini():
     return(render_template("gemini.html"))
+
 @app.route("/gemini_reply",methods=["GET","POST"])
 def gemini_reply():
     q = request.form.get("q")
     print(q)
     r = model.generate_content(q)
     return(render_template("gemini_reply.html",r=r.text))
+
 if __name__ == "__main__":
     app.run()
-
